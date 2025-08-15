2564cdd08ba8c27d18af1e5d49d48a4ede5e758e
Yogarth <yogarthkankheria@gmail.com>
2025-08-15 19:25:42 +0530

    'her'

--- src/pages/DynamicProductPage.jsx
+++ src/pages/DynamicProductPage.jsx
@@ -458,7 +458,7 @@ export const DynamicProductPage = () => {
                         <div>
                             <h4 className="font-semibold text-[#493657] text-lg mb-2">Safety Data Sheets</h4>
                             <ul className="list-disc pl-6 text-[#493657]/80 space-y-2">
-                                <li><a href={`/json-viewer.html?src=${encodeURIComponent(product.PIS)}`}  target="_blank" rel="noopener noreferrer" className="underline">SDS 1</a></li>
+                                <li><a href={`/pis-viewer.html?src=${encodeURIComponent(product.PIS)}`}  target="_blank" rel="noopener noreferrer" className="underline">SDS 1</a></li>
                             </ul>
                         </div>
                         <div>
